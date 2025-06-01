e999ff0e54f67dfb5d537c0d6ff9ae448e2f70a1
Alsami10 <165448323+Alsami10@users.noreply.github.com>
2025-06-01 10:16:28 +0000

    Java, defineitely hard but trying

---
+++ .vscode/frontend/backend/AppointmentScheduler.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+public class AppointmentScheduler {
+    public static final String URL = "jdbc:mysql://localhost:3306/appointment_db";
+    Public Static Final String USER = "Root";
+    Public Static 
+    }
+    
+
+
---
+++ .vscode/frontend/database.sql
@@ -0,0 +1,31 @@
+create database appointment_db;
+use appointment_db;
+
+Create Table Doctors (
+     doctor_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    Speciality VARCHAR(50) NOT NULL,
+    Availability TEXT NOT NULL
+
+);
+Create Table Appointments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    appointment_date DATE NOT NULL,
+    appointment_time TIME NOT NULL,
+    doctor_id INT NOT NULL,
+    Status VARCHAR(20) DEFAULT 'Booked',
+    FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id)
+);
+
+select @@secure_file_priv;
+
+Load DATA INFILE '/path/to/Doctors_dataset.csv'
+INTO TABLE Doctors
+Fields Terminated By ','
+Enclosed By '"'
+Lines Terminated By '\n'
+Ignore 1 Rows;
+
+Select * from Doctors where Speciality = 'Cardiologist';
+
